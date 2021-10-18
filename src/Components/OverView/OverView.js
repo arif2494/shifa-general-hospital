646c7960efb1a58e180451d0b94a4c85e517d73d
Ariful Islam <arifulislam161024@gmail.com>
2021-10-18 12:48:53 +0600

    overview section done

--- src/Components/OverView/OverView.js
+++ src/Components/OverView/OverView.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import patient from '../../img/icons/patient.png';
+import doctor from '../../img/icons/doctor.png';
+import nurse from '../../img/icons/nurse.png';
+import lab from '../../img/icons/test-tube.png';
 const OverView = () => {
 	return (
-		<div>
-			<div>
-				<h2>Over View</h2>
+		<div className="overView">
+			<div className="container mx-auto">
+				<div className="grid-cols-4 grid  justify-items-center">
+					<div className="mt-24  flex items-center flex-col justify-center ">
+						<img src={patient} alt="!" className="w-20" />
+						<p className="text-4xl font-medium text-white mt-2">5K Patient Treated</p>
+					</div>
+					<div className="mt-24  flex items-center flex-col justify-center ">
+						<img src={doctor} alt="!" className="w-20" />
+						<p className="text-4xl font-medium text-white mt-2">50 Doctor</p>
+					</div>
+					<div className="mt-24  flex items-center flex-col justify-center ">
+						<img src={nurse} alt="!" className="w-20" />
+						<p className="text-4xl font-medium text-white mt-2">76 Staff</p>
+					</div>
+					<div className="mt-24  flex items-center flex-col justify-center ">
+						<img src={lab} alt="!" className="w-20" />
+						<p className="text-4xl font-medium text-white mt-2">3 Stablished Lab</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -5,7 +5,7 @@ import Service from '../Service/Service';
 const Services = () => {
 	const { services } = useService();
 	return (
-		<div className="bg-gray-200">
+		<div className="bg-gray-200 pb-6">
 			<div className="container mx-auto">
 				<h2 className="text-black py-3 px-6 mb-3 text-4xl  rounded-fulls text-center my-6">
 					Our Different type of services
